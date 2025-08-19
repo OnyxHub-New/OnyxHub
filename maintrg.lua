abb4057f02751c31add10e62ad425a9def471c02
OnyxHub-New <shitarohelp@gmail.com>
2025-08-19 21:52:34 +0300

    Create maintrg.lua

---
+++ maintrg.lua
@@ -0,0 +1,24 @@
+--[[script made by actor(onyxhub)]]--
+--[[please read what is written below in the comments!!!]]--
+--[[
+set ownertag to your twink name
+]]--
+local main = loadstring(game:HttpGet("https://raw.githubusercontent.com/OnyxHub-New/OnyxHub/refs/heads/main/bhtest.lua"))()
+--loading main script
+
+main:SetOwnerTag("OnyxHubLol1") --ownertag
+main:SetHealthCheck(true) --set health check true or false
+main:Init() --init script
+
+--[[
+commands:
+"!tp me" or over nickname - teleport to player
+"!kill Nickname" - kill player
+"!kaura Nickname" - kill aura on the target
+"!unkaura" - stop kill aura or "!kaura"
+"!stomp Nickname" - stomp player
+]]--
+
+--[[
+teams will be replenished wait! in the meantime join our telegram channel - t.me/OnyxHub
+]]--
